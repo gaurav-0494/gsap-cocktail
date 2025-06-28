cd72f558ae3ea402f4b08fa50ee79b19ccd9c236
Gaurav <gaurav0494.be21@chitkara.edu.in>
2025-06-28 12:10:51 +0530

    about section implementation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,6 +40,7 @@ const Hero = () => {
         })
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
+            .to(".arrow", { y: 100 }, 0);
 
         const startValue = isMobile ? 'top 50%' : 'center 60%';
         const endValue = isMobile ? '120% top' : 'bottom top';
@@ -72,6 +73,7 @@ const Hero = () => {
               <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
 
               <div className="body">
+                  <img src="/images/arrow.png" alt="arrow" className="arrow" />
                   <div className="content">
                       <div className="space-y-5 hidden md:block">
                           <p>Cool. Crisp. Classic</p>
